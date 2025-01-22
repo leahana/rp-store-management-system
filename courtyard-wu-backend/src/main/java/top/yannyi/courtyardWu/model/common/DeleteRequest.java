ff26934f5ff021c7d9207986c1d3897b853cca17
leah_ana <53849184+leahana@users.noreply.github.com>
2025-01-22 10:47:56 +0800

    [backend]后端项目类补充

---
+++ courtyard-wu-backend/src/main/java/top/yannyi/courtyardWu/model/common/DeleteRequest.java
@@ -0,0 +1,16 @@
+package top.yannyi.courtyardWu.model.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+
+}
